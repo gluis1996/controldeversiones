6f4fc7b029bcbe75b95b2c9078281403f71a80f5
gonza <gonza@DESKTOP-05P4MTN>
2021-10-07 16:50:34 -0500

    PROGRAME BOTON DE LOGIN

---
+++ src/USUARIOS.java
@@ -0,0 +1,12 @@
+
+public class USUARIOS {
+    String user;
+    String password;
+
+    public USUARIOS(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+    
+    
+}
